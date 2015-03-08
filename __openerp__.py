09783cb1410953acf31d19af6ccfc663b6d45749
sniper7kills <willg@1337hax.org>
2015-03-08 11:41:48 -0400

    Very Basic Bootstrap layout

--- __openerp__.py
+++ __openerp__.py
@@ -1,20 +1,22 @@
 {
-    # Theme information
-    'name': "DCN's Clean Theme",
-    'description': """
-    """,
-    'category': 'Theme',
-    'version': '1.0',
-    'depends': ['website'],
+	# Theme information
+	'name': "DCN's Clean Theme",
+	'description': """
+	""",
+	'category': 'Theme',
+	'version': '1.4',
+	'depends': ['website'],
 
-    # templates, pages, and snippets
-    'data': [
-        'views/pages.xml',
-        #'views/options.xml',
-        #'views/snippets.xml',
-    ],
+	# templates, pages, and snippets
+	'data': [
+		'views/pages.xml',
+		'views/layout.xml',
+		'views/theme.xml',
+		#'views/options.xml',
+		#'views/snippets.xml',
+	],
 
-    # Your information
-    'author': "sniper7kills@DynamicCode.Ninja",
-    'website': "http://www.dynamiccode.ninja",
+	# Your information
+	'author': "sniper7kills@DynamicCode.Ninja",
+	'website': "http://www.dynamiccode.ninja",
 }
